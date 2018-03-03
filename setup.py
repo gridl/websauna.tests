472963e03c6122e7fe0bca870dcd710cb24b4fbf
Érico Andrei <ericof@gmail.com>
2018-03-03 15:05:37 +0100

    Back to development: 1.0a9

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 
 setup(
     name='websauna.tests',
-    version='1.0a8',
+    version='1.0a9.dev0',
     url='https://github.com/websauna/websauna.tests',
     author='Mikko Ohtamaa',
     author_email='mikko@opensourcehacker.com',
